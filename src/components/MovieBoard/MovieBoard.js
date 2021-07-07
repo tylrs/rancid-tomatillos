d93d92dff9998ca5a2aea4356f0863ea082423fd
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-07 15:30:51 -0600

    Add MovieCover import and added it to MovieBoard

--- src/components/MovieBoard/MovieBoard.js
+++ src/components/MovieBoard/MovieBoard.js
@@ -1,16 +1,13 @@
 import React from "react"
+import MovieCover from "../MovieCover/MovieCover"
 
- const MovieBoard = (props) => {
-    
+ const MovieBoard = ({movies}) => {
+    let movieCovers = movies.map(movie => <MovieCover key={movie.id} poster={movie.poster_path}/>)
   return (
-
-  )
+    <section>
+      {movieCovers}
+    </section>
+   )
  }
  
-
-
-
-
-
-
 export default MovieBoard
