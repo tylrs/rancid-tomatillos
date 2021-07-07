b5f494ed4710ffe2f8b2f16da2dd7458d34f63af
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-07 16:02:59 -0600

    Add title props

--- src/components/MovieBoard/MovieBoard.js
+++ src/components/MovieBoard/MovieBoard.js
@@ -2,7 +2,7 @@ import React from "react"
 import MovieCover from "../MovieCover/MovieCover"
 
  const MovieBoard = ({movies}) => {
-    let movieCovers = movies.map(movie => <MovieCover key={movie.id} poster={movie.poster_path}/>)
+    let movieCovers = movies.map(movie => <MovieCover key={movie.id} poster={movie.poster_path} title={movie.title}/>)
   return (
     <section>
       {movieCovers}
