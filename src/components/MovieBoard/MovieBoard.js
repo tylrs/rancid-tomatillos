8f93614265108465ed7916369ac492dfa75c9605
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-07 16:25:59 -0600

    Import styling into components

--- src/components/MovieBoard/MovieBoard.js
+++ src/components/MovieBoard/MovieBoard.js
@@ -1,5 +1,6 @@
 import React from "react"
 import MovieCover from "../MovieCover/MovieCover"
+import "./MovieBoard.css"
 
  const MovieBoard = ({movies}) => {
         let movieCovers = movies.map(movie => <MovieCover key={movie.id} id={movie.id} poster={movie.poster_path} title={movie.title}/>)
