de0bfc31876261b59bfc60ae939ea13c313d5314
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-08 16:00:23 -0600

    Add API error handling

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import movieData from '../../movieData'
+// import movieData from '../../movieData'
 import MovieBoard from '../MovieBoard/MovieBoard'
 import Movie from '../Movie/Movie'
 import React,{ Component } from 'react';
@@ -9,14 +9,16 @@ class App extends Component {
     super() 
       this.state = {
         movies: [],
-        selectedMovie: {}
+        selectedMovie: {},
+        error: ''
       }
     }
 
     componentDidMount() {
       fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then((response) => response.json())
-      .then((movieData) => this.setState({movies: movieData.movies}))
+      .then(response => response.json())
+      .then(movieData => this.setState({movies: movieData.movies}))
+      .catch(error => this.setState({error: 'Oops server is down!'}))
     }
 
   
@@ -39,8 +41,9 @@ class App extends Component {
         <header className='app-title'>
           <h1>Rancid Tomatillos</h1>
         </header>
+        {this.state.error && <p>Whoopsie, the server is down!</p> }
         {
-          this.state.selectedMovie.title ? 
+          this.state.selectedMovie.title && !this.state.error ?
           <Movie key={this.state.selectedMovie.id} movieInfo={this.state.selectedMovie} unselectMovie={this.unselectMovie}/> : 
           <MovieBoard movies={this.state.movies} selectMovie={this.selectMovie}/>
         }
