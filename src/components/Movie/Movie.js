1bf29078a7b9ac860f1611729b7819aa715d7814
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-07 18:56:18 -0600

    Add all data into selected movie article

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -4,12 +4,12 @@ const Movie = (props) => {
     let {backdrop_path, title, average_rating,
          release_date, overview, genres, budget,
           revenue, runtime, tagline} = props;
-    // let genreTags = genres    
+    let genreTags = genres.map(genre => <p>{genre}</p>)   
     return (
         <article className='selected-movie'>
             <img src={backdrop_path} alt={title}/>
             <p>Release Date:<span>{release_date}</span></p>
-
+            <p>Genres:<span>{genreTags}</span></p>
             <div>
                 <p>Rating:<span>{average_rating}</span></p>
                 <p>Duration:<span>{runtime} mins</span></p>   
