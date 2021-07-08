372733f2e8e95201d2a879906992798330a82d18
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-08 15:39:11 -0600

    Refactor this.state to use movie data from API

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,10 +8,17 @@ class App extends Component {
   constructor() {
     super() 
       this.state = {
-        movies: movieData.movies,
+        movies: [],
         selectedMovie: {}
       }
     }
+
+    componentDidMount() {
+      fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then((response) => response.json())
+      .then((movieData) => this.setState({movies: movieData.movies}))
+    }
+
   
   selectMovie = (id) => {
     const selectedMovie = this.state.movies.find(movie => movie.id === id)
