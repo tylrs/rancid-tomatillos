04307d10c8d163f946a6ead9d8a075dfbf0f5ca8
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-07 18:53:24 -0600

    Add skeleton for selected movie

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Movie = (props) => {
+    let {backdrop_path, title, average_rating,
+         release_date, overview, genres, budget,
+          revenue, runtime, tagline} = props;
+    // let genreTags = genres    
+    return (
+        <article className='selected-movie'>
+            <img src={backdrop_path} alt={title}/>
+            <p>Release Date:<span>{release_date}</span></p>
+
+            <div>
+                <p>Rating:<span>{average_rating}</span></p>
+                <p>Duration:<span>{runtime} mins</span></p>   
+            </div>
+            <hr />
+            <h2>{overview}</h2>
+            <p>Budget:<span>${budget}</span></p>
+            <p>Revenue:<span>${revenue}</span></p>
+            <p>Tagline:<span>${tagline}</span></p>
+        </article>
+    )
+}
+
+export default Movie
