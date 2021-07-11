42ac4a5ca1188da433a77140a4efcdb5fdbf6ec5
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-11 14:14:28 -0600

    Fix scss pathing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './index.scss'
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
