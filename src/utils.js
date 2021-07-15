a3e497996c72bfb54e5ff702a3a23b05b5845a91
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-14 19:25:53 -0600

    Add clean single movie fetch function

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,27 @@
 export const cleanMovies = (movies) => {
     return movies.map(movie => {
-        return ({
+        return {
             id: movie.id,
             title: movie.title,
             poster_path: movie.poster_path
-        })
+        }
     })
+}
+
+export const cleanMovie = (selectedMovie) => {
+    const {backdrop_path, title, average_rating,
+        release_date, overview, genres, budget,
+         revenue, runtime, tagline} = selectedMovie;
+    return {
+        backdrop_path, 
+        title, 
+        average_rating,
+        release_date, 
+        overview, 
+        genres, 
+        budget,
+        revenue, 
+        runtime, 
+        tagline
+    }
 }
