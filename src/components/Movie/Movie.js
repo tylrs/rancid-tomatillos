49961bce02c57608599b87723eaadf979220cdd3
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-15 16:09:06 -0600

    Add is required to proptypes

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -60,14 +60,14 @@ class Movie extends Component {
 export default Movie
 
 Movie.propTypes = {
-    movieInfo: PropTypes.object,
-    title: PropTypes.string,
-    average_rating: PropTypes.number,
-    release_date: PropTypes.string,
-    overview: PropTypes.string,
-    genres: PropTypes.array,
-    budget: PropTypes.number,
-    revenue: PropTypes.number,
-    runtime: PropTypes.number,
-    tagline: PropTypes.string
+    movieInfo: PropTypes.object.isRequired,
+    title: PropTypes.string.isRequired,
+    average_rating: PropTypes.number.isRequired,
+    release_date: PropTypes.string.isRequired,
+    overview: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    budget: PropTypes.number.isRequired,
+    revenue: PropTypes.number.isRequired,
+    runtime: PropTypes.number.isRequired,
+    tagline: PropTypes.string.isRequired
 }
