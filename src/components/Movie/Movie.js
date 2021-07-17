58b7a34937f3ef3a9a51ea4417bc5d1da41164f9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-17 11:30:41 -0600

    Refactor movie render's layout

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,61 +1,68 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { sortGenres } from '../../utils'
 import PropTypes from 'prop-types';
 
 class Movie extends Component {
-    componentDidMount() {
-       this.props.selectMovie(this.props.id)
-    }
-
-    render() {
-        const {backdrop_path, title, average_rating,
-            release_date, overview, genres = [], budget,
-             revenue, runtime, tagline} = this.props.movieInfo;
-       let genreTags = genres.map((genre, index) => <p key={index}>{genre}</p>)
-       return (
-          <article className='selected-movie'>
-            <div className='title-container'>
-              <h2>{title}</h2>
-              <h3>{tagline}</h3>
-            </div>
-            <div className="img-container">
-              <img src={backdrop_path} alt={title} />
-              <div className="date-container">
-                <h4>Release Date:</h4>
-                <p>{release_date}</p>
-              </div>
-              <Link to='/'><button onClick={()=> {this.props.unselectMovie()}}>back</button></Link> 
-            </div>
+  componentDidMount() {
+    this.props.selectMovie(this.props.id)
+  }
+  render() {
+      const {backdrop_path, title, average_rating,
+          release_date, overview, genres = [], budget,
+            revenue, runtime, tagline} = this.props.movieInfo;
+      let genreTags = sortGenres(genres)
+      return (         
+        <article className='selected-movie'>
+        <div className="img-container">
+          <img src={backdrop_path} alt={title} />
+          <div className="tagline-container">
+            <h3>{tagline}</h3>
+          </div>
+        </div>
+        <div className='title-container'>
+          <h2>{title}</h2>
+          <Link to='/'><button className="movie-back-button" onClick={()=>
+            {this.props.unselectMovie()}}>back</button></Link>
+        </div>
+        <div className="info-container">
+          <div className="rating-container">
+            <h4>Rating:</h4>
+            <p>{average_rating}</p>
+          </div>
+          <div className="date-container">
+            <h4>Release Date:</h4>
+            <p>{release_date}</p>
+          </div>
+        </div>
+        <div className="full-detail-container">
+          <div className="detail-container">
             <h4>Genres:</h4>
-            <div className="genre-container">
-            {genreTags}
-            </div>
-            <div className="detail-container">
-              <h4>Rating:</h4>
-              <p>{average_rating}</p>
-              <h4>Duration:</h4>
-              <p>{runtime} mins</p>
-            </div>
-            <div className="detail-container">
-              {!!budget && 
-              <>
-                <h4>Budget:</h4>
-                <p>${budget}</p>
-              </>
-              }
-              {!!revenue && 
-              <>
-                <h4>Revenue:</h4>
-                <p>${revenue}</p>
-              </>}
-            </div>
-            <hr />
-            <p className="overview">{overview}</p>
-          </article>
-       )
-    }
-    
-}
+            <div className="genre-container"> {genreTags} </div>
+          </div>
+          <div className="detail-container">
+            <h4>Duration:</h4>
+            <p>{runtime} mins</p>
+          </div>
+          {!!budget && 
+          <div className="detail-container">
+            <h4>Budget:</h4>
+            <p>${budget}</p>
+          </div> }
+          {!!revenue &&
+          <div className="detail-container">
+            <h4>Revenue:</h4>
+            <p>${revenue}</p>
+          </div> }
+        </div>
+      {!!overview &&
+      <>
+        <hr />
+        <p className="overview">{overview}</p>
+        </>}
+      </article> 
+    )}
+  }
 
 export default Movie
 
