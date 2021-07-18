ac60102547f38dde79bba077756688f29b3b63bf
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-18 13:22:30 -0600

    Add first test for favorites user flow

--- cypress/integration/favorite_movie_spec.js
+++ cypress/integration/favorite_movie_spec.js
@@ -1,3 +1,22 @@
 describe('Favorite Movies User Flow', () => {
-    it('Should show an empty heart when first visiting movie details ')
+    const singleMovieReq = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919'
+    const favoriteMoviesReq = 'http://localhost:3001/favorites'
+
+    it('Should show an empty heart when first visiting movie details', () => {
+        cy.intercept('GET', singleMovieReq, {
+            statusCode: 200,
+            fixture: 'movie'
+          }) 
+        cy.intercept('GET', favoriteMoviesReq, {
+            statusCode: 200,
+            response: {
+                "favorites": []
+            }
+        }) 
+        cy.LoadHome()
+        .get('a[href="/movies/694919"]')
+        .click()
+        cy.get('.svg-inline--fa')
+        .should('have.css', 'background-color', 'rgba(147, 178, 190, 0.8)')
+    })
 })
