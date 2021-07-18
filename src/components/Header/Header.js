d3e4f5e55c87885a18e185762baa875939739ea0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-18 10:06:53 -0600

    Add NavLinks for favorites and back

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faBackward } from '@fortawesome/free-solid-svg-icons'
+
 
 const Header = () => {
   return (
     <header>
+
   <NavLink to="/">
-      <h1>Rancid Tomatillos</h1>
+  <FontAwesomeIcon icon={faBackward} />
+  </NavLink>
+  <NavLink to="/">
+  <h1>Rancid Tomatillos</h1>
+  </NavLink>
+  <NavLink to="/favorites">
+  <FontAwesomeIcon icon={faHeart} />
   </NavLink>
   {/* <NavLink to="/favorites" /> */}
     </header>
